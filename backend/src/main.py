b872454f44235bc1dea50071e094d81249a00e3f
lissk0 <105405909+LiSSk0@users.noreply.github.com>
2025-03-31 20:21:43 +0300

    Изменена структура проекта. Добавлены файлы .env, .env.example, requirements.txt

---
+++ backend/src/main.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import sys
+import os
+
+# Загружаем переменные окружения из .env:
+load_dotenv()
+
+# Получаем учетные данные для БД:
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = "db_calendar"  # os.getenv("DB_NAME")
+
+
+if __name__ == '__main__':
+    print(DB_USER, DB_PASSWORD, DB_NAME)
+    # Проверка корректности данных для БД:
+    if DB_USER is None or len(DB_USER) <= 0:
+        print("# Bad credentials. Exiting the program.")
+        sys.exit()
+
+    # app.run(debug=True)
