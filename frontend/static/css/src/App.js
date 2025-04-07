61642d5ab0a2311e89b2a06e4e15c9b352f071bf
Mary <masha.mashchalkina@mail.ru>
2025-04-08 02:42:10 +0400

    Update App.js
    основной компонент приложения

--- frontend/static/css/src/App.js
+++ frontend/static/css/src/App.js
@@ -1,1 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
+import Navbar from './components/Navbar';
 
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
