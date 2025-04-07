3d3ba304ac1f5b8e560a49e7e94b4a29fc2a7127
Mary <masha.mashchalkina@mail.ru>
2025-04-08 02:48:25 +0400

    Update ProfilePage.js

--- frontend/static/css/src/pages/ProfilePage.js
+++ frontend/static/css/src/pages/ProfilePage.js
@@ -1,39 +1,55 @@
 import React from 'react';
+import Profile from '../components/Profile';
+import Modal from '../components/Modal';
+import AddVacationForm from '../components/AddVacationForm';
 
-const Calendar = ({ vacations, selectedDepartment }) => {
-  // Фильтруем отпуска по выбранному отделу
-  const filteredVacations = selectedDepartment
-    ? vacations.filter(vacation => vacation.department === selectedDepartment)
-    : vacations;
+const ProfilePage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
+  // Временные данные пользователя для демонстрации
+  const [user, setUser] = useState({
+    email: 'user@example.com',
+    name: 'Иван',
+    surname: 'Иванов',
+    middlename: 'Иванович',
+    position: 'Разработчик',
+    nextVacation: {
+      start: '2025-07-15',
+      end: '2025-07-30',
+    },
+  });
+
+  const handleUpdate = (updatedData) => {
+    setUser({
+      ...user,
+      ...updatedData,
+    });
+  };
+
+  const handleAddVacation = (vacationData) => {
+    console.log('Добавлен отпуск:', vacationData);
+    setIsModalOpen(false);
+  };
 
   return (
-    <div className="calendar">
-      <table>
-        <thead>
-          <tr>
-            <th>ФИО</th>
-            <th>Отпуск</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredVacations.map(vacation => (
-            <tr key={vacation.id}>
-              <td>{vacation.name}</td>
-              <td>
-                <div
-                  className="vacation-bar"
-                  style={{ backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(16)}` }}
-                >
-                  {/* Отображение даты отпуска */}
-                  {vacation.fromDate} - {vacation.toDate}
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="profile-page">
+      <Profile user={user} onUpdate={handleUpdate} />
+      
+      <button 
+        className="add-vacation-btn" 
+        onClick={() => setIsModalOpen(true)}
+      >
+        Добавить отпуск
+      </button>
+      
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <AddVacationForm 
+          onSubmit={handleAddVacation} 
+          onCancel={() => setIsModalOpen(false)} 
+        />
+      </Modal>
     </div>
   );
 };
 
-export default Calendar;
+export default ProfilePage;
